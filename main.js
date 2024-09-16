68e8960b88fd8db250dc74628a10686d5efd6b25
starzeta <mario37@abv.bg>
2024-09-16 16:20:58 +0300

    update

--- main.js
+++ main.js
@@ -1,9 +1,34 @@
+// page fade-out
 document.addEventListener("DOMContentLoaded", () => {
-  const hamburger = document.getElementById("hamburger");
-  const navMenu = document.getElementById("navMenu");
+  const links = document.querySelectorAll("a[data-link]");
 
-  hamburger.addEventListener("click", () => {
-    navMenu.classList.toggle("active");
-    hamburger.classList.toggle("active");
+  links.forEach((link) => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const href = link.getAttribute("href");
+
+      if (href !== "#") {
+        document.body.classList.add("fade-out");
+        setTimeout(() => {
+          window.location.href = href;
+        }, 300);
+      }
+    });
+  });
+});
+
+window.addEventListener("pageshow", (event) => {
+  if (event.persisted) {
+    document.body.classList.remove("fade-out");
+  }
+});
+
+// hamburger
+document.addEventListener("DOMContentLoaded", function () {
+  const hamburger = document.querySelector(".hamburger");
+  const nav = document.querySelector(".nav");
+
+  hamburger.addEventListener("click", function () {
+    nav.classList.toggle("active");
   });
 });
